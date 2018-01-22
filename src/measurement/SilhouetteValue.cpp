18e241535f7dc4e2ca670d6b29ae09fb7deec4fd
Philipp Spohr <spohr.philipp@web.de>
2018-01-22 15:06:34 +0100

    Fixes to the Silhouette Value calculation

--- src/measurement/SilhouetteValue.cpp
+++ src/measurement/SilhouetteValue.cpp
@@ -18,7 +18,6 @@ namespace ysk{
 
 	double SilhouetteValue::getValue(){
 
-
 		//Treat negative values as offsets OR zeros?
 		const bool negativeToZero = false;
 		const bool negativeOffset = true;
@@ -34,7 +33,7 @@ namespace ysk{
 			if (verbosity > 4) cout << "Minimum edge weight: " << minimumEdgeWeight <<endl;
 		}
 
-		//Calculate average dissimilarities to own clusters (a(i))
+		//Calculate average dissimilarities to own cluster (a(i))
 		map<int,double> _averageDissimilaritiesOwn = map<int,double>();
 		for(auto const &cluster : _solution){
 			for (auto const &node : cluster){
@@ -55,21 +54,13 @@ namespace ysk{
 						sum += edgeWeight;
 					}
 				}
-				//cout << dissim << " " << sum << endl;
 				dissim *= sum;
+				if (verbosity > 4){
+					cout << "a("<< node << ")= " << dissim << endl;
+				}
 				_averageDissimilaritiesOwn[node]=dissim;
-
 			}
 		}
-
-//		if (verbosity > 5){
-//			cout << "Average Dissimilarities to own clusters" << endl << endl;
-//			for (auto const &cluster : _solution){
-//				for (auto const &node: cluster){
-//					cout << node << " : " << _averageDissimilaritiesOwn[node]<< endl;
-//				}
-//			}
-//		}
 
 		//Calculate average dissimilarities to other clusters (b(i))
 		map<int,double> _averageDissimilaritiesForeign= map<int,double>();
@@ -83,6 +74,9 @@ namespace ysk{
 				//Sum Factor
 				double sum = 0.0;
 				for (auto const &otherCluster : _solution){
+					//Skip internal edges
+					if (otherCluster == cluster) continue;
+
 					for (auto const &otherNode : otherCluster){
 						FullGraph::Edge edge = _instance->getOrig().findEdge(
 								_instance->getOrig().nodeFromId(node),
@@ -98,31 +92,30 @@ namespace ysk{
 					}
 				}
 				dissim *= sum;
+				if (verbosity > 4){
+					cout << "b("<< node << ")= " << dissim << endl;
+				}
 				_averageDissimilaritiesForeign[node]=dissim;
 
 			}
 		}
 
-//		if (verbosity > 4){
-//			cout << "Average Dissimilarities to other clusters" << endl << endl;
-//			for (auto const &cluster : _solution){
-//				for (auto const &node: cluster){
-//					cout << node << " : " << _averageDissimilaritiesForeign[node]<< endl;
-//				}
-//			}
-//		}
-
 		//Calculate silhouettes
 		map<int,double> _silhouettes= map<int,double>();
 		for (auto const &cluster: _solution){
 			for (auto const &node : cluster){
+				double max = std::max(_averageDissimilaritiesForeign[node],_averageDissimilaritiesOwn[node]);
 				_silhouettes[node] =
-						(1.0/_instance->getSize())*
+						(_averageDissimilaritiesForeign[node] == 0 && _averageDissimilaritiesOwn[node] == 0) ? //Both a(i) and b(i) are 0
+								0 : //Special case: We set it to 0 -> Avoid division by 0
 						(
 							(_averageDissimilaritiesForeign[node] -_averageDissimilaritiesOwn[node])
 							/
-							(std::max(_averageDissimilaritiesForeign[node],_averageDissimilaritiesOwn[node]))
+							max
 						);
+				if (verbosity > 4){
+					cout << "s("<< node << ")= " << _silhouettes[node] << endl;
+				}
 			}
 		}
 
